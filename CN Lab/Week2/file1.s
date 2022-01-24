1997e6ed4082c2782686844c5d34cac4d4ab40e4
Naman <appleinc2341@gmail.com>
2022-01-24 15:23:19 +0530

    Some files

---
+++ CN Lab/Week2/file1.s
@@ -0,0 +1,15 @@
+
+.text
+        
+        MOV r1, #3
+        ANDs r2, r1, #1
+        
+        
+        BEQ condition
+                MOV r0, #0xFF
+                B exit
+        condition:
+                MOV r0, #0x00
+        exit:
+                SWI 0x011
+.end
